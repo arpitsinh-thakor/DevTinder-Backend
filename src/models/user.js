faa2fbfe72ee20af3f582fb44097efea0d4eef4a
arpitsinh-thakor <1803arpitthakor@gmail.com>
2024-09-20 18:57:31 +0530

    Data sanitization and schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,80 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName : {
         type: String,
-        required: true
+        required: true,
+        minLength: 3,
+        maxLength: 50,
+        trim: true
     },
     lastName : {
         type: String,
-        required: true
+        required: true,
+        minLength: 3,
+        maxLength: 50,
+        trim: true
     },
     email : {
         type: String,
-        required: true
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 320,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email - " + value);
+            }
+        }
     },
     password : {
         type: String,
-        required: true
+        required: true,
+        trim: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Invalid Password - " + value);
+            }
+        }
+    },
+    age :{
+        type: Number,
+        min: 18,
     },
     gender : {
         type: String,
-        required: true
+        trim: true,
+        validate(value){
+            if(!["male", "female", "others"].includes(value.toLowerCase())){
+                throw new Error("Invalid Gender Type - " + value);
+            }
+        }
     },
+    photoUrl : {
+        type: String,
+        default: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png',
+        trim: true,
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL - " + value);
+            }
+        }
+    },
+    about : {
+        type: String,
+        maxLength : 50,
+        minLength : 3,
+    },
+    skills : {
+        type: [String],
+        maxLength: 10,
+    },
+},
+{
+    timestamps: true
 });
 
 module.exports = mongoose.model('User', userSchema);

93a0c14c3280b5c7f8147bc7133d8ffa4db154f3
arpitsinh-thakor <1803arpitthakor@gmail.com>
2024-09-24 19:31:51 +0530

    userRouter - feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -69,4 +69,53 @@ userRouter.get('/user/requests/connections', userAuth, async (req, res) => {
     }
 })
 
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+    try{
+        const user = req.user;
+        let page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+        limit = limit > 30 ? 30 : limit;
+        if(!user){
+            throw new Error("User not found");
+        }
+        if(page < 1 || limit < 1){
+            throw new Error("Invalid page or limit");
+        }
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {sender: user._id},
+                {receiver: user._id}
+            ]
+        }).select('sender receiver');
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach(connectionRequest => {
+            hideUsersFromFeed.add(connectionRequest.sender.toString());
+            hideUsersFromFeed.add(connectionRequest.receiver.toString());
+        });
+
+        const users = await User.find({
+            $and: [
+                {_id: {$nin: Array.from(hideUsersFromFeed)}},
+                {_id: {$ne: user._id}}
+            ]
+        }).select(USER_SAFE_FIELDS)
+          .skip((page - 1) * limit)
+          .limit(limit);
+
+        if(!users){
+            res.send({message: "No users found"});
+        }
+
+        res.send({
+            message: "Feed fetched successfully",
+            users
+        });
+    }
+    catch(err){
+        res.status(400).send("Error while fetching feed: " + err);
+    }
+})
+
 module.exports = userRouter;
